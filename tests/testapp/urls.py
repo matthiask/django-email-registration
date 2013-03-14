5de09b5d283772110829be72ff6cdb32af79f416
Matthias Kestenholz <mk@spinlock.ch>
2013-03-14 11:36:57 +0100

    Add a simple testsuite

---
+++ tests/testapp/urls.py
@@ -0,0 +1,17 @@
+import os
+
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.http import HttpResponse
+
+
+admin.autodiscover()
+
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', lambda request: HttpResponse(repr(request.REQUEST))),
+    url(r'^ac/', include('django.contrib.auth.urls')),
+    url(r'^er/', include('email_registration.urls')),
+) + staticfiles_urlpatterns()
