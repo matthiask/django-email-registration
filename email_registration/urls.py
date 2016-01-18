4c4458910d1f251d1a480f75760289454f79b9c0
Matthias Kestenholz <mk@feinheit.ch>
2016-01-18 12:22:57 +0100

    Modernize urls.py

--- email_registration/urls.py
+++ email_registration/urls.py
@@ -1,12 +1,19 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-
-urlpatterns = patterns(
-    'email_registration.views',
-    url(r'^$',
-        'email_registration_form',
-        name='email_registration_form'),
-    url(r'^(?P<code>[^/]+)/$',
-        'email_registration_confirm',
-        name='email_registration_confirm'),
+from email_registration.views import (
+    email_registration_form, email_registration_confirm
 )
+
+
+urlpatterns = [
+    url(
+        r'^$',
+        email_registration_form,
+        name='email_registration_form',
+    ),
+    url(
+        r'^(?P<code>[^/]+)/$',
+        email_registration_confirm,
+        name='email_registration_confirm',
+    ),
+]
